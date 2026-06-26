e1c1645418a41a435ebae7e77199a95a3299597c
chorny-znich <vargar.dt@gmail.com>
2026-06-27 02:14:53 +0300

    feat: add adventure screen

---
+++ adventure_screen.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "game_data.h"
+#include <disreality_engine.h>
+
+class AdventureScreen : public dr::Screen
+{
+public:
+	void init() override;
+	void handleInput(const sf::Event& event, sf::RenderWindow& window) override;
+	void update(float dt) override;
+	void render(sf::RenderWindow& window) override;
+private:
+	struct ScreenInputVisitor;
+
+	sf::View mMainView{ sf::FloatRect({0, 0}, {gd::GraphicsResolution.x, gd::GraphicsResolution.y}) };
+};
+
