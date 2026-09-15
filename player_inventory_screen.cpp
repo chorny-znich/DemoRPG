e1f671d7243a919120db49b02229d9e360fec784
chorny-znich <vargar.dt@gmail.com>
2026-09-15 11:32:33 +0300

    feat: show player's equipment window

--- player_inventory_screen.cpp
+++ player_inventory_screen.cpp
@@ -4,18 +4,26 @@
 
 PlayerInventoryScreen::PlayerInventoryScreen()
 {
-  mPanel.setFillColor(PANEL_COLOR);
-  mPanel.setPosition(POSITION);
-  //createInventoryCells();
+  mInventoryPanel.setFillColor(INVENTORY_PANEL_COLOR);
+  mInventoryPanel.setPosition(INVENTORY_PANEL_POSITION);
+  mEquipmentPanel.setFillColor(EQUIPMENT_PANEL_COLOR);
+  mEquipmentPanel.setPosition(EQUIPMENT_PANEL_POSITION);
+  createInventoryCells();
+  createEquipmentCells();
 }
 
 void PlayerInventoryScreen::render(sf::RenderTarget& target)
 {
   if (mVisible)
   {
-    target.draw(mPanel);
+    target.draw(mInventoryPanel);
+    for (const auto& cell : mInventoryCells) 
+    {
+      cell.render(target);
+    }
 
-    for (const auto& cell : mCells) 
+    target.draw(mEquipmentPanel);
+    for (const auto& cell : mEquipmentCells)
     {
       cell.render(target);
     }
@@ -24,7 +32,6 @@ void PlayerInventoryScreen::render(sf::RenderTarget& target)
 
 void PlayerInventoryScreen::show()
 {
-  createInventoryCells();
   mVisible = true;
 }
 
@@ -41,16 +48,26 @@ bool PlayerInventoryScreen::isVisible() const
 void PlayerInventoryScreen::createInventoryCells()
 {
   int index{ 0 };
-  mCells.clear();
+  mInventoryCells.clear();
 
   for (int row{ 0 }; row < INVENTORY_SIZE.y; row++)
   {
     for (int col{ 0 }; col < INVENTORY_SIZE.x; col++)
     {
-      float x{ mPanel.getPosition().x + CELL_SHIFT + col * (CELL_SIZE.x + CELL_SHIFT) };
-      float y{ mPanel.getPosition().y + CELL_SHIFT + row * (CELL_SIZE.y + CELL_SHIFT) };
-      mCells.push_back(InventoryCell(index, { x, y }));
+      float x{ mInventoryPanel.getPosition().x + CELL_SHIFT + col * (CELL_SIZE.x + CELL_SHIFT) };
+      float y{ mInventoryPanel.getPosition().y + CELL_SHIFT + row * (CELL_SIZE.y + CELL_SHIFT) };
+      mInventoryCells.push_back(InventoryCell(index, { x, y }));
       index++;
     }
   }
+}
+
+void PlayerInventoryScreen::createEquipmentCells()
+{
+  mEquipmentCells.push_back(InventoryCell(1, { mEquipmentPanel.getPosition().x + 100.f, 
+    mEquipmentPanel.getPosition().y + 100.f }));
+  mEquipmentCells.push_back(InventoryCell(2, { mEquipmentPanel.getPosition().x + CELL_SHIFT, 
+    mEquipmentPanel.getPosition().y + 200.f }));
+  mEquipmentCells.push_back(InventoryCell(0, { mEquipmentPanel.getPosition().x + CELL_SHIFT, 
+    mEquipmentPanel.getPosition().y + CELL_SHIFT }));
 }
--- player_inventory_screen.h
+++ player_inventory_screen.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "inventory_cell.h"
+#include "game_data.h"
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
@@ -22,15 +23,24 @@ private:
   static inline constexpr sf::Vector2i INVENTORY_SIZE{ 10, 5 };
   static inline constexpr sf::Vector2f CELL_SIZE{ 50.f, 50.f };
   static inline constexpr float CELL_SHIFT{ 5.f };
-  static inline constexpr sf::Vector2f PANEL_SIZE{ INVENTORY_SIZE.x * CELL_SIZE.x + (INVENTORY_SIZE.x + 1) * CELL_SHIFT,
+  static inline constexpr sf::Vector2f INVENTORY_PANEL_SIZE{ INVENTORY_SIZE.x * CELL_SIZE.x + (INVENTORY_SIZE.x + 1) * CELL_SHIFT,
     INVENTORY_SIZE.y * CELL_SIZE.y + (INVENTORY_SIZE.y + 1) * CELL_SHIFT };
-  static inline constexpr sf::Vector2f POSITION{ 500.f, 400.f };
-  static inline constexpr sf::Color PANEL_COLOR{ 139, 69, 19, 225 };
+  static inline constexpr sf::Vector2f INVENTORY_PANEL_POSITION{ 500.f, 400.f };
+  static inline constexpr sf::Color INVENTORY_PANEL_COLOR{ 139, 69, 19, 225 };
 
-  sf::RectangleShape mPanel{ PANEL_SIZE };
+  const sf::Vector2i EQUIPMENT_SIZE{ 1, 3 };
+  static inline constexpr sf::Vector2f EQUIPMENT_PANEL_SIZE{ 250.f, 400.f };
+  static inline const sf::Vector2f EQUIPMENT_PANEL_POSITION{ gd::GraphicsResolution.x / 2 - EQUIPMENT_PANEL_SIZE.x / 2 + 250.f,
+    gd::GraphicsResolution.y / 2 - EQUIPMENT_PANEL_SIZE.y / 2 };
+  static inline constexpr sf::Color EQUIPMENT_PANEL_COLOR{ 0, 0, 0, 225 };
+
+  sf::RectangleShape mInventoryPanel{ INVENTORY_PANEL_SIZE };
+  sf::RectangleShape mEquipmentPanel{ EQUIPMENT_PANEL_SIZE };
   bool mVisible{ false };
 
-  std::vector<InventoryCell> mCells;
+  std::vector<InventoryCell> mInventoryCells;
+  std::vector<InventoryCell> mEquipmentCells;
 
   void createInventoryCells();
+  void createEquipmentCells();
 };
